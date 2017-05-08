093d2bdf30c91989f9788980929868f388b0a981
Brandon Villa <bornofos@gmail.com>
2017-05-08 09:21:09 -0500

    Letter changed

--- resources/views/wedding-layouts/couple.blade.php
+++ resources/views/wedding-layouts/couple.blade.php
@@ -2,9 +2,9 @@
   <div class="container">
     <div class="row">
       <div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
-        <h2>Hello!</h2>
-        <h3>November 28th, 2016 New York, USA</h3>
-        <p>We invited you to celebrate our wedding</p>
+        <h2>Hola!</h2>
+        <h3>28 de Noviembre, 2016 Ciudad Guzmán, México</h3>
+        <p>Te invitamos a celebrar nuestra boda</p>
       </div>
     </div>
     <div class="couple-wrap animate-box">
@@ -13,17 +13,17 @@
           <img src="images/groom.jpg" alt="groom" class="img-responsive">
         </div>
         <div class="desc-groom">
-          <h3>Joefrey Mahusay</h3>
+          <h3>Omar Moreno</h3>
           <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove</p>
         </div>
       </div>
       <p class="heart text-center"><i class="icon-heart2"></i></p>
       <div class="couple-half">
         <div class="bride">
-          <img src="images/bride.jpg" alt="groom" class="img-responsive">
+          <img src="images/omar-cheli/cheli.jpg" alt="groom" class="img-responsive">
         </div>
         <div class="desc-bride">
-          <h3>Sheila Mahusay</h3>
+          <h3>Araceli Esteban</h3>
           <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove</p>
         </div>
       </div>
