b3f00d21e6b62f69e9580e7fa850418f61600e84
Brandon Villa <bornofos@gmail.com>
2017-05-09 14:53:41 -0500

    Little changes

--- resources/views/wedding-layouts/nav.blade.php
+++ resources/views/wedding-layouts/nav.blade.php
@@ -7,23 +7,23 @@
       <div class="col-xs-9 text-right menu-1">
         <ul>
           <li class="active"><a href="index.html">Home</a></li>
-          <li><a href="about.html">Story</a></li>
+          <li><a href="about.html">Hisotria</a></li>
           <li class="has-dropdown">
-            <a href="services.html">Services</a>
-            <ul class="dropdown">
+            <a href="services.html">Mesa de regalos</a>
+            {{-- <ul class="dropdown">
               <li><a href="#">Web Design</a></li>
               <li><a href="#">eCommerce</a></li>
               <li><a href="#">Branding</a></li>
               <li><a href="#">API</a></li>
-            </ul>
+            </ul> --}}
           </li>
           <li class="has-dropdown">
             <a href="gallery.html">Gallery</a>
             <ul class="dropdown">
-              <li><a href="#">HTML5</a></li>
-              <li><a href="#">CSS3</a></li>
-              <li><a href="#">Sass</a></li>
-              <li><a href="#">jQuery</a></li>
+              <li><a href="#">Galeria1</a></li>
+              <li><a href="#">Galeria2</a></li>
+              <li><a href="#">Galeria3</a></li>
+              <li><a href="#">Galeria4</a></li>
             </ul>
           </li>
           <li><a href="contact.html">Contact</a></li>
--- resources/views/wedding/qrcode.blade.php
+++ resources/views/wedding/qrcode.blade.php
@@ -4,7 +4,8 @@
     <div class="row">
       <div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
         <div class="text-center">
-          {!! QrCode::size(250)->margin(2)->color(219,45,126)->generate(Request::url()); !!}
+          {!! QrCode::size(250)->margin(2)->color(219,45,126)->generate('http://www.simplesoftware.io'); !!}
+
         </div>
         <p>A quick easy way to share your wedding with your loved ones!</p>
       </div>
